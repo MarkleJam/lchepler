07493086b51679407e430bfd422c53062d91bd49
Zhiquan Zhang <zzq20y@gmail.com>
2020-05-25 16:35:02 -0400

    Add customed sql

---
+++ app/sqls/sql.js
@@ -0,0 +1,6 @@
+module.exports = {
+    searchReview : "select item.id, item.name,  item.type1, item.type2,item.type3, item.diff, item.grasp, max(date) as md " +
+    "from item " +
+    "inner join history on item.id=history.problemid " +
+    "group by problemid having DATEDIFF(now(), md) > {{interval}}",
+};
