737c6c10d36b2c60546ba0f7ae46be936d5f77be
Zhiquan Zhang <zzq20y@gmail.com>
2020-05-25 22:16:34 -0400

    return createAt and add include equal date

--- app/sqls/sql.js
+++ app/sqls/sql.js
@@ -1,6 +1,6 @@
 module.exports = {
-    searchReview : "select item.id, item.name,  item.type1, item.type2,item.type3, item.diff, item.grasp, max(date) as md " +
+    searchReview : "select item.id, item.name,  item.type1, item.type2,item.type3, item.diff, item.grasp, item.createAt, max(date) as md " +
     "from item " +
     "inner join history on item.id=history.problemid " +
-    "group by problemid having DATEDIFF(now(), md) > {{interval}}",
+    "group by problemid having DATEDIFF(now(), md) >= {{interval}}",
 };
